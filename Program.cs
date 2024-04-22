b3c8107e5d7272758a5c78aae389e589c2c601b6
MissC1010 <celina.indhur@hotmail.com>
2024-04-22 22:42:53 +0200

    git push
    git push

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using TB_Tutorial;
+
+Unit player = new Unit(100, 20, 12, "player");
+Unit enemy = new Unit(75, 10, 7, "enemy");
+Random random = new Random();
+
+while (!player.IsDead && !enemy.IsDead)
+{
+    Console.WriteLine($"{player.UnitName} HP = {player.Hp}, {enemy.UnitName} HP = {enemy.Hp} ");
+
+    Console.WriteLine("Player turn! What will you do?");
+    string choice = Console.ReadLine();
+
+    if (choice == "a")
+    {
+        player.Attack(enemy);
+    }
+    else
+    {
+        player.Heal();
+    }
+
+    if (player.IsDead || enemy.IsDead)
+    {
+        break;
+    }
+
+    Console.WriteLine($"{player.UnitName} HP = {player.Hp}, {enemy.UnitName} HP = {enemy.Hp} ");
+
+    Console.WriteLine($"enemy turn");
+
+    int rand = random.Next(0, 2);
+
+    if (rand == 0)
+    {
+        enemy.Attack(player);
+    }
+    else
+    {
+        enemy.Heal();
+    }
+}
+
---
+++ Unit.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TB_Tutorial
+{
+    internal class Unit
+    {
+        //fields
+        private int currentHp;
+        private int maxHp;
+        private int attackPower;
+        private int healPower;
+        private string unitName;
+        private Random random;
+
+        //properties
+        public int Hp { get { return currentHp; } }
+        public string UnitName { get { return unitName; } }
+        public bool IsDead { get { return currentHp <= 0; } }
+
+        //constructor
+        public Unit(int maxHp, int attackPower, int healPower, string unitName)
+        {
+            this.maxHp = maxHp;
+            this.currentHp = maxHp;
+            this.attackPower = attackPower;
+            this.healPower = healPower;
+            this.unitName = unitName;
+            this.random = new Random();
+        }
+
+        //methods 
+        public void Attack(Unit unitToAttack)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int randDamage = (int)(attackPower * rng);
+            unitToAttack.TakeDamage(randDamage);
+            Console.WriteLine($"{unitName} attacks {unitToAttack.unitName} and deals {randDamage} damage");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHp -= damage;
+
+            if (IsDead)
+            {
+                Console.WriteLine($"{UnitName} has been defeated");
+            }
+        }
+
+        public void Heal()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int heal = (int)(rng * healPower);
+            currentHp = heal + currentHp > maxHp ? maxHp : currentHp + heal;
+            Console.WriteLine($"{UnitName} heals {heal}");
+
+        }
+    }
+}
+
